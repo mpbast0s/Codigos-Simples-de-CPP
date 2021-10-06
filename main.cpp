e65578ab5058d219e80cb6c4df73cd96b2cd4217
Maria Paula Bastos <50062780+mpbast0s@users.noreply.github.com>
2021-10-06 09:23:56 -0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,13 @@
+/* 
+ Arquivo para testar
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+cout << "✨✨✨\n  Para fazer rodar um dos exercicios tem a descrição de como no final de cada um deles. \n✨✨✨" << endl;
+return  0;
+}
